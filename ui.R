8d45f843abd49c8c596d3430f4ee922a10c7032b
Phil Bowsher <indyprb@rstudio.com>
2021-06-24 15:26:41 +0000

    update title

--- ui.R
+++ ui.R
@@ -3,7 +3,7 @@
 library(shiny)
 
 fluidPage(
-    titlePanel("Adverse Events Query to OpenFDA", windowTitle = "Adverse Events Query to OpenFDA"),
+    titlePanel("Adverse Events Query to OpenFDA Games of Thrones", windowTitle = "Adverse Events Query to OpenFDA Phils Game in Thrones"),
     fluidRow(
         column(3,
             br(),
